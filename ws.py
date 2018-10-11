af4205e061484fe6611c26f3c747823d84a017db
Ward, Joel <jward@conversantmedia.com>
2018-10-11 16:51:04 -0700

    improve

--- ws.py
+++ ws.py
@@ -1,9 +1,18 @@
-import os, sys, time, bottle, mimetypes
-mimetypes.add_type('application/wasm','wasm')
-ROOT='.'
+import os, bottle, mimetypes
+
+mimetypes.add_type('application/wasm','.wasm')
+
+def make_static_mount(root):
+    def static_file(path):
+        if os.path.isdir(path):
+            bottle.redirect(os.path.join('/',path,'index.html'))
+        return bottle.static_file(path, root)
+    return static_file
+
+static_mount = make_static_mount('.')
+
 @bottle.get('/')
 @bottle.get('/<path:path>')
-def _(path = ROOT):
-    if os.path.isdir(path):
-        bottle.redirect(os.path.join('/',path,'index.html'))
-    return bottle.static_file(path, ROOT)
+@bottle.get('/<path:path>/')
+def _(path = ''):
+    return static_mount(path)
